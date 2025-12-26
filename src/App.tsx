e2069199c154b7f48602a042389e2460cfe39290
Kacper Żmuda <zmudakacper03@gmail.com>
2025-12-26 10:40:09 +0100

    Basic validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
 function App() {
 	const today = new Date();
 	today.setMinutes(today.getMinutes() - today.getTimezoneOffset());
+
 	const maxDate = today.toISOString().split("T")[0];
 
 	return (
 		<>
 			<form id="pin-form">
 				<div>
-					<label htmlFor="name">Imię noworodka:</label>
-					<input type="text" id="name" name="name" placeholder="np. Jan" required minLength={2} />
+					<label htmlFor="name">Imię i nazwisko noworodka:</label>
+					<input
+						type="text"
+						id="name"
+						name="name"
+						placeholder="np. Jan"
+						minLength={2}
+						onKeyDown={e => {
+							if (e.key === " ") e.preventDefault();
+						}}
+						required
+					/>
+					&nbsp;
+					<input
+						type="text"
+						id="name"
+						name="name"
+						placeholder="np. Kowalski"
+						minLength={2}
+						onKeyDown={e => {
+							if (e.key === " ") e.preventDefault();
+						}}
+						required
+					/>
 				</div>
 
 				<fieldset>
@@ -20,23 +43,25 @@ function App() {
 					</label>
 
 					<label>
-						<input type="radio" name="gender" value="female" />
+						<input type="radio" name="gender" value="female" required />
 						Kobieta
 					</label>
 				</fieldset>
 
 				<div>
-					{/* Połączyć birth-date i birth-time */}
 					<label htmlFor="birth-date">Data i godzina narodzin noworodka:</label>
-					<input type="date" id="birth-date" name="birthDate" min="2025-12-24" max={maxDate} required />
+					<input type="date" id="birth-date" name="birthDate" max={maxDate} required />
+					&nbsp;
+					<input type="time" id="birth-date" name="birthTime" required />
 				</div>
 
-				<div>
-					<label htmlFor="birth-time">Godzina narodzin noworodka:</label>
-					<input type="time" id="birth-time" name="birthTime" required />
-				</div>
-
-				<button type="submit">Generuj PIN</button>
+				<button
+					type="submit"
+					onSubmit={e => {
+						e.preventDefault();
+					}}>
+					Generuj PIN
+				</button>
 			</form>
 		</>
 	);
